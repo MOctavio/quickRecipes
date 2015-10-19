435c130ffe4a254911ac669e5a62455eedcfa39d
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-19 13:29:14 -0600

    created complex objects for search input and buld scaffold for go to search button

--- app/controllers/home/HomeController.js
+++ app/controllers/home/HomeController.js
@@ -1,5 +1,10 @@
 angular.module('knowlEDGE.controllers')
     .controller('HomeController', ['$scope', function($scope) {
         $scope.selected = undefined;
-        $scope.documents = ['Ice cream sandwich','Ice cream recipes', 'Ice cream history', 'Cherry pie recipe', 'Cherry pie history'];
+        $scope.documents = [{id: 0, label: 'Ice cream sandwich'},{id: 1, label: 'Ice cream recipes'},
+            {id: 2, label: 'Ice cream history'},{id: 3, label: 'Cherry pie recipe'},{id: 4, label: 'Cherry pie history'}];
+
+        this.goToSerachView = function () {
+            console.log("goToSerachView, " + $scope.selected);
+        };
     }]);
