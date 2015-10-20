06e6f2a6bd7d695171bcb64b601e4ed3abd00625
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-20 08:52:22 -0600

    impleted new methods

--- app/services/search/SearchService.js
+++ app/services/search/SearchService.js
@@ -1,2 +1,32 @@
 angular.module('knowlEDGE.services')
-    .service('SearchService', [function() { }]);
+    .service('SearchService', [function() {
+        var activeSearchElement = {};
+
+        this.setActiveSearchElement = function (document) {
+            activeSearchElement = document;
+        };
+
+        this.getActiveSearchElement = function () {
+            return activeSearchElement;
+        };
+
+        this.getDocumentList = function() {
+            return [{
+                id: 0,
+                label: 'Ice cream sandwich'
+            }, {
+                id: 1,
+                label: 'Ice cream recipes'
+            }, {
+                id: 2,
+                label: 'Ice cream history'
+            }, {
+                id: 3,
+                label: 'Cherry pie recipe'
+            }, {
+                id: 4,
+                label: 'Cherry pie history'
+            }];
+        };
+
+    }]);
