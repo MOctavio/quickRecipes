0ea164ee3c83253144c4111fcba6a70a58e01ba8
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-20 09:03:01 -0600

    included redirection to home page in nav brand

--- app/controllers/search/SearchController.js
+++ app/controllers/search/SearchController.js
@@ -1,10 +1,13 @@
 angular.module('knowlEDGE.controllers')
-    .controller('SearchController', ['$scope','SearchService', function($scope,SearchService) {
+    .controller('SearchController', ['$scope','$location','SearchService', function($scope,$location,SearchService) {
         $scope.selected = undefined;
         $scope.documents = [{id: 0, label: 'Ice cream sandwich'},{id: 1, label: 'Ice cream recipes'},
             {id: 2, label: 'Ice cream history'},{id: 3, label: 'Cherry pie recipe'},{id: 4, label: 'Cherry pie history'}];
 
         this.goToSerachView = function () {
             console.log("goToSerachView, " + $scope.selected);
+        };
+        this.redirectTo = function (url) {
+            if (url) $location.url(url);
         };
     }]);
