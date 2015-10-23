6d9a00b51cccbcd94ec0a764b69d30f731ec7155
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-23 15:38:08 -0600

    get results method implemented

--- app/services/search/SearchService.js
+++ app/services/search/SearchService.js
@@ -9,6 +9,58 @@ angular.module('knowlEDGE.services')
         this.getActiveSearchElement = function() {
             return activeSearchElement;
         };
+        this.getSearchResults = function() {
+            // TODO: Server side implementation.
+            // the activeSearchElement should be sent
+            // to the server to get a result list.
+            return [{
+                title: "Ice cream sandwich history",
+                description: "This document is about ice cream sandwich history in San Jose, Costa Rica.",
+                recomendation: "Sharable with clients to know how to easily make ice cream sandwichs",
+                documentId: 27091990,
+                timestamp: 1441366472000,
+                downloadcount: 30,
+                rating: 3,
+                ratingCount: 5,
+                commentsCount: 4,
+                authorslist: [{
+                    "id": 1,
+                    "username": "jazagoury",
+                    "enabled": true,
+                    "firstName": "Jessica",
+                    "lastName": "Azagoury",
+                    "picture": "assets/images/user-1.jpg",
+                    "email": "jessica.azagoury@hpe.com"
+                }]
+            }, {
+                title: "Ice cream sandwich recipes",
+                description: "This document contains 10 ice cream sandwich recipes to do at home.",
+                recomendation: "Sharable with clients to know how to easily make ice cream sandwichs.",
+                documentId: 27091980,
+                timestamp: 1445589272000,
+                downloadcount: 55,
+                rating: 5,
+                ratingCount: 25,
+                commentsCount: 10,
+                authorslist: [{
+                    "id": 1,
+                    "username": "jazagoury",
+                    "enabled": true,
+                    "firstName": "Jessica",
+                    "lastName": "Azagoury",
+                    "picture": "assets/images/user-1.jpg",
+                    "email": "jessica.azagoury@hpe.com"
+                }, {
+                    "id": 2,
+                    "username": "bvargas",
+                    "enabled": true,
+                    "firstName": "Bryan",
+                    "lastName": "Vargas",
+                    "picture": "assets/images/user-2.jpg",
+                    "email": "bryan.vargas@hpe.com"
+                }]
+            }];
+        };
 
         this.getItemsList = function() {
             return [{
@@ -32,7 +84,7 @@ angular.module('knowlEDGE.services')
             }, {
                 id: 6,
                 label: 'Ice cream desserts'
-            },{
+            }, {
                 id: 7,
                 label: 'Ice cream cake'
             }, {
