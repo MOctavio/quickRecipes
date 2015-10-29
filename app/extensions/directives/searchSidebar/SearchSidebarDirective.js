4d767ece59bb759020b14aa34986e4edbd68508d
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-29 14:20:25 -0600

    new sidebar directive

---
+++ app/extensions/directives/searchSidebar/SearchSidebarDirective.js
@@ -0,0 +1,26 @@
+angular.module('knowlEDGE.directives')
+    .directive('searchSidebar', [function() {
+        return {
+            scope: {
+                options: '=?'
+            },
+            restrict: 'E',
+            templateUrl: 'extensions/directives/searchSidebar/_searchSidebarTemplate.html',
+            controller: ['$scope', '$location', 'SearchService', function($scope, $location, SearchService) {
+
+                $scope.searchCount = SearchService.getResultsCount();
+                $scope.searchFilters = SearchService.getSearchFilters();
+
+                this.redirectTo = function(url) {
+                    if (url) $location.url(url);
+                };
+                $scope.hoveringOver = function(value) {};
+                $scope.favorite = function(item) {
+                    item.favorite = !item.favorite;
+                };
+
+            }],
+            controllerAs: 'searchSidebar',
+            link: function($scope, element, attrs, controller) {}
+        };
+    }]);
